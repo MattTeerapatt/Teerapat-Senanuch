53d56bedd41428e4050ce5919a06e4f3738e4602
MattTeerapatt <mmatt.trp@gmail.com>
2023-12-06 10:35:25 +0700

    fix(deploy): photos not found

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Teerapat-Senanuch/',
   plugins: [react()],
-  assetsInclude: ['**/*.mov', '**/*.mp4']
+  assetsInclude: ['**/*.mov', '**/*.mp4, **/*.png, **/*.jpg, **/*.jpeg, **/*.gif, **/*.svg'],
 })
